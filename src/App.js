501c3b5f18030a53dab84684d523b325c15bb53d
Bruno Silva_CONTR <c0603070@vale.com>
2020-11-01 13:51:04 -0200

    Adicionando mais de um subcomponente

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Header />
+    </div>
+  );
 }
-
 export default App;
