a3743380acc8abe042c40f456c8b932c4f9098ec
Bruno Silva_CONTR <c0603070@vale.com>
2020-11-03 00:44:05 -0200

    Importando imagens e arquivos css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import "./App.css";
+import backgroundImage from "./assets/tech-glass.jpg";
+
 import Header from "./components/Header";
 
 function App() {
@@ -16,6 +19,8 @@ function App() {
   return (
     <>
       <Header title='Projects' />
+
+      <img src={backgroundImage} width={300} />
 
       <ul>
         {projects.map(project => (
